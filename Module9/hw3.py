6d3bbf8cd0b1e2f11b3c9024df3fea29d695cabe
dankas112 <daniilxxl3@gmail.com>
2024-07-23 02:24:19 +0700

    Module9 hw 2, 3, 4 complete

---
+++ Module9/hw3.py
@@ -0,0 +1,10 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+first_result = (len(x[0]) - len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
+second_result = (len(x[0]) == len(x[1]) for x in ((first[i], second[i]) for i in range(len(first))))
+
+print(list(first_result))
+print(list(second_result))
+
+
